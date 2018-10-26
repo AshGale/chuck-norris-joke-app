c35469628d7d50fdbfe6dab323564073ffcf52b7
AshGale <ashgale17@yahoo.co.uk>
2018-10-26 10:20:59 +0100

    add xml configuration

--- src/main/java/web/boot/guru/chucknorrisjokeapp/ChuckNorrisJokeAppApplication.java
+++ src/main/java/web/boot/guru/chucknorrisjokeapp/ChuckNorrisJokeAppApplication.java
@@ -2,8 +2,10 @@ package web.boot.guru.chucknorrisjokeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class ChuckNorrisJokeAppApplication {
 
     public static void main(String[] args) {
